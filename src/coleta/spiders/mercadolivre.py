d6788f96cbda1e63340bcb6b3512c6d9128f3abf
Valdemir Machado <valdemir.nmachado@gmail.com>
2024-05-26 16:23:54 -0300

    First commit

--- src/coleta/spiders/mercadolivre.py
+++ src/coleta/spiders/mercadolivre.py
@@ -3,7 +3,6 @@ import scrapy
 
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
-    allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
 
     def parse(self, response):
@@ -12,16 +11,16 @@ class MercadolivreSpider(scrapy.Spider):
         for product in products:
 
             prices = product.css('span.andes-money-amount__fraction::text').getall() 
-            cents = product.css('span.andes-money-amount__fraction::text').getall() 
+            cents = product.css('span.andes-money-amount__cents::text').getall() 
 
             yield {
 
                 'brand' : product.css('span.ui-search-item__brand-discoverability.ui-search-item__group__element::text').get(),
                 'name'  : product.css('h2.ui-search-item__title::text').get(),
                 'old_price_reais' : prices[0] if len(prices) > 0 else None,
-                'old_price_reais' : cents[0] if len(cents) > 0 else None,
-                'new_price_reais' : prices[1] if len(prices) > 0 else None,
-                'new_price_reais' : cents[1] if len(cents) > 0 else None,
+                'old_price_centavos' : cents[0] if len(cents) > 0 else None,
+                'new_price_reais' : prices[1] if len(prices) > 1 else None,
+                'new_price_centavos' : cents[1] if len(cents) > 1 else None,
                 'reviews_rating_number' : product.css('span.ui-search-reviews_rating-number::text').get(),
                 'reviews_amount' : product.css('span.ui-search-reviews_amount::text').get()
             }
