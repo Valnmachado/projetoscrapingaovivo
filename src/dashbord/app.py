3362974ae8aa08c8a5b95d4bf337054391a05f64
Valdemir Machado <valdemir.nmachado@gmail.com>
2024-05-26 21:22:55 -0300

    Adicionando funcionalidade no streamlit dashbord

--- src/dashbord/app.py
+++ src/dashbord/app.py
@@ -31,4 +31,19 @@ col2.metric(label="Número de Marcas Únicas", value=unique_brands)
 average_new_price = df['new_price'].mean()
 col3.metric(label="Preço Médio Novo (R$)", value=f"{average_new_price:.2f}")
 
+# Quais marcas são mais encontradas até a 10ª página
+st.subheader('Marcas mais encontradas até a 10ª página')
+col1, col2 = st.columns([4, 2])
+top_10_pages_brands = df['brand'].value_counts().sort_values(ascending=False)
+col1.bar_chart(top_10_pages_brands)
+col2.write(top_10_pages_brands)
+
+# Qual o preço médio por marca
+st.subheader('Preço médio por marca')
+col1, col2 = st.columns([4, 2])
+average_price_by_brand = df.groupby('brand')['new_price'].mean().sort_values(ascending=False)
+col1.bar_chart(average_price_by_brand)
+col2.write(average_price_by_brand)
+
+# Qual a satisfação por marca
 
